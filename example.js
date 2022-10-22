662f2959cfed965b5c54664f75c0664f106257c4
Nikita Suyazov <jumper211212@gmail.com>
2022-10-22 11:08:16 -0700

    Delete example.js

--- example.js
+++
@@ -1,6 +0,0 @@
-const findElementPosition = (matrix, number) => {
-  const rowIndex = matrix.findIndex((row) => row.includes(number));
-  const columnIndex = matrix[rowIndex].indexOf(number);
-
-  return [rowIndex, columnIndex];
-};
